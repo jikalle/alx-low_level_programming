a6ffaf5ad8563162d81786d7ff5f242d76f96e7e
Zunnurayn <shehuusman1414@gmail.com>
2022-09-09 00:06:40 +0100

    Script that print some text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#includ <stdio.h>
+/**
+ * main-servers as the entry point for the program
+ *
+ * return xero if no errors else return non-zero value
+ */
+in main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return(0)
+}
