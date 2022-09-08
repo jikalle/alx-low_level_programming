783c37250a5179ce0ffe7f14b3d9aba64da73726
Zunnurayn <shehuusman1414@gmail.com>
2022-09-09 00:37:27 +0100

    Script that print with proper grammar...

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * this is the main-entry point
+ *
+ * Return:0 if no errors, return non-zero if error
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
