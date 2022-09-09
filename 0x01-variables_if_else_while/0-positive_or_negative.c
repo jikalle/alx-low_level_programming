57c66626ae7a6005e40315d653d64cae9a226b5e
Zunnurayn <shehuusman1414@gmail.com>
2022-09-09 22:02:33 +0100

    Print if else while system

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes here*/
 
 /* betty style doc for function main goes there*/
@@ -19,7 +20,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes here*/
 	if (n < 0);
-		printf("%d is negative\n", n);
+		printf("%d" is negative\n, n);
 	else if (n == 0);
 		printf("%d is zero\n", n);
 	else
