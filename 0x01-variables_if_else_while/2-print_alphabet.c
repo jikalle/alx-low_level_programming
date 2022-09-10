680a27cc9a861b7db9d473d3c8d80a8e54e850ba
Zunnurayn <shehuusman1414@gmail.com>
2022-09-10 11:39:30 +0100

    Print letters a to z in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
