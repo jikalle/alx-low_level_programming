3ca52d1f0163162a9fe45df7cbfd4dd079354f54
Zunnurayn <shehuusman1414@gmail.com>
2022-09-10 16:39:29 +0100

    Print combination of numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,20 +3,20 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: always 0 (success)
  */
 int main(void)
 {
 	int n;
+
 	rand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes here*/
-	if (n > 0);
+
+	if (n > 0)
 		printf("%d is positivee\n", n);
-	else if (n == 0);
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-		printf("%d is else\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
