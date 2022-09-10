007a6982b897e72a51925591b4846e736a250360
Zunnurayn <shehuusman1414@gmail.com>
2022-09-10 16:24:49 +0100

    Print combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+	putchar(number + '0');
+	if (number < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n')'
+	return (0);
+}
