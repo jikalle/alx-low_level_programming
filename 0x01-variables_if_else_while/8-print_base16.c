c1b54ab57d60ecec9cdd757446129eabe2604603
Zunnurayn <shehuusman1414@gmail.com>
2022-09-10 13:52:55 +0100

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		} else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+	
