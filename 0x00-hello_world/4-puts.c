6558e946a7598259a547af47a43fa52a51db3ae3
Zunnurayn <shehuusman1414@gmail.com>
2022-09-12 19:29:09 +0100

    Repair betty doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - program that prints  "Programming is like building a multilingual puzzle"
+ * main - program "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
