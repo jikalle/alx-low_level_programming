0705c42a715bdb658a28ae4d2414ee0864630729
Zunnurayn <shehuusman1414@gmail.com>
2022-09-15 18:06:58 +0100

    Print largest number of 3

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Debugging Infinte loop avoided \\o/\n
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+/**
+ *	int = 0;
+ *
+ *	while (i = 10)
+ *	{
+ *		putchar(i);
+ *	}
+ */	
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
