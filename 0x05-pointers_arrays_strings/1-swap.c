3475bb97f4e2642824bde9c103d752b1269d054f
Zunnurayn <shehuusman1414@gmail.com>
2022-09-19 17:12:55 +0100

    Swap int a t b

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * swap - swap to interger's value
+ * swap_int - swap to interger's value
  * @a: takes the value of 98
  * @b: takes the value of 42
  * Return: void
  */
-void swap(int a, int b)
+void swap_int(int *a, int *b)
 {
-	int a;
-	int b;
+	int c = *a;
 
-	a = 98;
-	b = 42;
-
-	swap(a, b);
-	printf("a=%d, b=%d\n", a, b);
-	return (0);
+	*a = *b;
+	*b = c;
 }
