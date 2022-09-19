01c8b1ebd9ca50a9846e1ba451623b09112aefc2
Zunnurayn <shehuusman1414@gmail.com>
2022-09-19 16:44:56 +0100

    Takes pointer as argument and vhange teh alue to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to int as parameter
+ * @n: pointer reset
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is pointer project and pointer value manipulation
