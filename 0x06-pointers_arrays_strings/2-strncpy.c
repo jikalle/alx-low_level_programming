4a15f542c5543f11a6d97832fd7af34fe048e15d
Zunnurayn <shehuusman1414@gmail.com>
2022-09-22 16:24:24 +0100

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,12 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int byteCount;
+	int byteCount;
 
-for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
-dest[byteCount] = src[byteCount];
-for (byteCount < n; byteCount++)
-dest[byteCount] = '\0';
+	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
+		dest[byteCount] = src[byteCount];
+	for (byteCount < n; byteCount++)
+		dest[byteCount] = '\0';
 
-return (dest);
+	return (dest);
 }
