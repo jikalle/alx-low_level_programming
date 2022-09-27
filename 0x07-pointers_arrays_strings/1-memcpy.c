595e28a06c1f550cb8a16fdebf2eea605666a05b
Zunnurayn <shehuusman1414@gmail.com>
2022-09-27 10:38:43 +0100

    Copy memory address

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,16 +7,17 @@
 * @n: number of bytes
 * Return: pointer to dest
 */
-int *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
+
 int size = n;
 
-		if (size > 0)
-		{
-			int i;
+if (size > 0)
+{
+int i;
 
-			for (i = 0, i < size, i++)
-				dest[i] = src[i];
-		}
-		return (dest);
+for (i = 0, i < size, i++)
+dest[i] = src[i];
+}
+return (dest);
 }
