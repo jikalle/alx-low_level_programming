316835a9eb0d132093d41cddca22298582f1520b
Zunnurayn <shehuusman1414@gmail.com>
2022-09-28 20:36:37 +0100

    Print string in reverse

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#inlcude "main.h"
+#include <stdio.h>
+
+/**
+* _strlen_recursion - Return the lenght of a string
+*
+* @s: string
+*
+* Return: void
+*/
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+
+return (0);
+s++;
+return (1 + _strlen_recursion(s));
+}
