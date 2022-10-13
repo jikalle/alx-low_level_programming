ec2679502e30f4334b6eb167687124bd3482c165
Zunnurayn <shehuusman1414@gmail.com>
2022-10-13 22:17:49 +0100

    Hotfix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,66 +1,23 @@
 #include "3-calc.h"
-/**
- * op_add - adds two integers
- * @a: first integer
- * @b: second integer
- * Return: sum of two integers
- */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-
 
 /**
- * op_sub - subtracts two integers
- * @a: first integer
- * @b: second integer
- * Return: difference of two integers
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
  */
-int op_sub(int a, int b)
+int main(int argc, char *argv[])
 {
-	return (a - b);
-}
+	register int a, b;
+	int (*fptr)(int, int);
 
-/**
- * op_mul - multiply two integers
- * @a: first integer
- * @b: second integer
- * Return: product of two integers
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - divides two integers
- * @a: first integer
- * @b: second integer
- * Return: quotient of two integers
- */
-int op_div(int a, int b)
-{
-	if (!b)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
-}
-
-/**
- * op_mod - calculates modulus two integers
- * @a: first integer
- * @b: second integer
- * Return: modulus of two integers
- */
-int op_mod(int a, int b)
-{
-	if (!b)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	fptr = get_op_func(argv[2]);
+	if (!fptr)
+		printf("Error\n"), exit(99);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%i\n", fptr(a, b));
+	return (0);
 }
